b8bec4d901c3d3a674a57e89549ebc388e4b1717
Matias <64340009+CineraM@users.noreply.github.com>
2023-10-08 01:46:08 -0400

    update

--- proj2.sql
+++ proj2.sql
@@ -20,9 +20,6 @@ create table player_rs_career (ilkid varchar(30), firstname varchar(30), lastnam
 
 create table player_rs (ilkid varchar(30), year int, firstname varchar(30), lastname varchar(30), tid varchar(30), league varchar(10), gp int, minutes int, pts int, dreb int, oreb int, reb int, asts int, stl int, blk int, turnover int, pf int, fga int, fgm int, fta int, ftm int, tpa int, tpm int, primary key(ilkid, year, tid));
 
-
--- Load all the above tables with data obtained from the corresponding txt file using the 'copy' command
--- Please remember to modify the path to your files accordingly
 \copy teams from '/home/matias/Documents/GitHub/sql_NBA_Queries/teams.txt' with delimiter ','
 \copy coaches_season from '/home/matias/Documents/GitHub/sql_NBA_Queries/coaches_season.txt' with delimiter ','
 \copy players from '/home/matias/Documents/GitHub/sql_NBA_Queries/players.txt' with delimiter ','
@@ -31,4 +28,61 @@ create table player_rs (ilkid varchar(30), year int, firstname varchar(30), last
 \copy draft from '/home/matias/Documents/GitHub/sql_NBA_Queries/draft.txt' with delimiter ','
 
 
--- Put your SQL statement after this line
+-- 1
+-- SELECT P.ilkid, P.firstname, P.lastname
+-- FROM players P, player_rs PR
+-- WHERE P.ilkid = PR.ilkid AND
+-- PR.tid IN (SELECT tid FROM teams WHERE location = 'Miami') AND
+-- P.ilkid NOT IN (SELECT PR2.ilkid FROM player_rs PR2 WHERE PR2.tid IN (SELECT tid FROM teams WHERE location = 'Dallas'));
+
+
+-- 2
+-- SELECT P.ilkid, P.lastname, P.firstname
+-- FROM players P
+-- WHERE P.first_season >= 2000
+-- AND P.last_season <= 2004
+-- ORDER BY P.lastname, P.firstname;
+
+
+-- 3
+-- skip for now!
+-- SELECT p.college AS college_name, COUNT(DISTINCT d.draft_year) / COUNT(DISTINCT p.first_season) AS avg_drafts_per_season
+-- FROM players p
+-- JOIN draft d ON p.ilkid = d.ilkid
+-- GROUP BY p.college
+-- HAVING COUNT(DISTINCT d.draft_year) >= 3
+
+
+-- 4
+-- WITH CoachTeamCount AS 
+-- (
+--     SELECT CS.cid, CS.firstname, CS.lastname, COUNT(DISTINCT CS.TID) AS team_count
+--     FROM coaches_season CS
+--     WHERE CS.year BETWEEN 1985 AND 1990
+--     GROUP BY CS.cid, CS.firstname, CS.lastname
+-- )
+
+-- SELECT CT.cid, CT.firstname, CT.lastname
+-- FROM CoachTeamCount CT
+-- WHERE CT.team_count = (SELECT MAX(team_count) FROM CoachTeamCount);
+
+
+-- 7
+-- SELECT firstname, lastname, (h_feet * 30.48 + h_inches * 2.54) AS height_cm
+-- FROM players
+-- ORDER BY height_cm
+-- LIMIT 5;
+
+
+-- 8 
+SELECT DISTINCT cs.cid, cs.firstname, cs.lastname
+FROM coaches_season cs
+WHERE NOT EXISTS (
+    SELECT DISTINCT t.league
+    FROM teams t
+    WHERE t.tid = cs.tid
+)
+ORDER BY cs.cid;
+
+
+
